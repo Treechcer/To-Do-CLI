ca47c4974cda985c68365528304274898f789ce3
Treecher <treechcer@gmail.com>
2025-09-02 20:03:42 +0200

    made some changes, change the text, added a way to cahnge the task things, fixed some bugs / things that were incorrect also, ver : 0.0.3

--- main.ps1
+++ main.ps1
@@ -1,4 +1,4 @@
-$version = "0.0.2"
+$global:version = "0.0.3"
 
 function startThis{
     
@@ -38,16 +38,92 @@ function startThis{
             else {
                 $JSON = Get-Content -Path (createPath $number) -Raw | ConvertFrom-Json
 
+                $JSON.tasks += (createTask $false 1250 "test" "test" $JSON $true)
                 fileWrite $JSON
 
                 writeOutTasks $JSON
+
+                editingJSON $JSON
             }
+        }
+        5 {
+
         }
         Default {
             Write-Host "incorrect input, starting again"
             startThis
         }
     }
+}
+
+function editingJSON {
+    param (
+        $JSON
+    )
+
+    while($true){
+        $number = Read-Host "Which task you want to modify?, 'none' as answer to any doesn't change the thing and 'end' in any stops this"
+
+
+        if ($number.ToLower() -eq "end") {
+            break
+        }
+        elseif ($number -gt $JSON.tasksNum){
+            Write-Host "This task doesn't exist"
+            continue
+        }
+
+        $isFinished = Read-Host "Is the task finished?"
+
+        if ($isFinished.ToLower() -eq "true"){
+            $isFinished = $true
+        }
+        elseif ($isFinished.ToLower() -eq "false"){
+            $isFinished = $false
+        }
+        elseif ($isFinished.ToLower() -eq "none") {
+            $isFinished = $JSON.tasks[$number].isFinished
+        }
+        elseif ($isFinished.ToLower() -eq "end") {
+            break
+        }
+        else {
+            Write-Host "it has to be true or false, restarting"
+            continue
+        }
+
+        $percentage = Read-Host "how many percets it's done?"
+
+        if (([int]$percentage) -gt 100){
+            $percentage = 100
+        }
+        elseif ($percentage.ToLower() -eq "none") {
+            $percentage = $JSON.tasks[$number].percentage
+        }
+        elseif ($percentage.ToLower() -eq "end") {
+            break
+        }
+        else {
+            $percentage = ([int] $percentage)
+        }
+
+        $name = Read-Host "what's the name of the task?"
+
+        if ($name.ToLower() -eq "none") {
+            $name = $JSON.tasks[$number].name
+        }
+        elseif ($name.ToLower() -eq "end") {
+            break
+        }
+        
+        $description = Read-Host "what's the description of the task?"
+
+        $JSON.tasks[$number - 1] = (createTask $isFinished $percentage $name $description $JSON $false)
+
+        fileWrite $JSON
+    }
+
+    writeOutStart
 }
 
 function fileWrite {
@@ -67,22 +143,24 @@ function writeOutTasks{
 
     foreach($task in $JSON.tasks){
         Write-Host ""
-        Write-Host "task number:" ([string]$task.id)
-        Write-Host "Name:" ([string]$task.name)
-        Write-Host "Description:" ([string]$task.description)
-        "-------"
+        Write-Host "--------------------------------------"
+        Write-Host "Task ID: $($task.id)"
+        Write-Host "Name: $($task.name)"
+        Write-Host "Description: $($task.description)"
+
         if ($task.isFinished){
-            Write-Host "Status: finished"
+            $status = "Completed"
         }
         else {
-            Write-Host "Status: not finished"
+            $status = "In Progress"
         }
+        Write-Host "Status: $status"
         
         $per = [int] ($task.percentage / 5)
-
         $out = "#" * $per -join ""
         $out += "-" * (20 - $per) -join ""
-        Write-Host "Percentage: $out"
+        Write-Host "Progress: [$out] $($task.percentage)%"
+        Write-Host "--------------------------------------"
         Write-Host ""
     }
 }
@@ -93,10 +171,23 @@ function createTask {
         [int16]$percentage,
         [string]$name,
         [string]$description,
-        [PSCustomObject]$JSON
+        [PSCustomObject]$JSON,
+        [bool]$changeID
     )
 
-    $id = $JSON.tasks[$JSON.tasks.Length - 1].id + 1
+    if ($changeID){
+        $JSON.tasksNum += 1
+    }
+
+    $id = $JSON.tasksNum
+
+    if ($percentage -gt 100){
+        $percentage = 100
+    }
+
+    if ($percentage -eq 100 -and $isFinished -eq $false){
+        $isFinished = $true
+    }
 
     return [PSCustomObject]@{
         isFinished = $isFinished
@@ -109,16 +200,17 @@ function createTask {
 
 function writeOutStart {
     Write-Host "--------------------------------------"
-    Write-Host "This app is small app for tasks, To-Do"
-    Write-Host "list and similar functions, that can"
-    Write-Host "help you with what you want to do"
-    Write-Host "and tracking what you want to do"
+    Write-Host "Welcome to your personal To-Do Manager!"
+    Write-Host ""
+    Write-Host "This small app helps you create, view, and manage"
+    Write-Host "your tasks efficiently. Track your progress and"
+    Write-Host "stay organized with ease."
     Write-Host "--------------------------------------"
     Write-Host ""
-    Write-Host "You can press 1 to make a new To-Do"
-    Write-Host "tasks"
-    Write-Host "You can press 2 to load JSON and change"
-    Write-Host "inside of the tasks"
+    Write-Host "Options:"
+    Write-Host "1 - Create a new To-Do list"
+    Write-Host "2 - Load an existing To-Do JSON and modify tasks"
+    Write-Host "5 - end"
     Write-Host ""
 
     startThis
